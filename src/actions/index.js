bd269d5d3bfe3b482894aab94e1b79123bf0baa2
mucida <lucas.mucida@ufv.br>
2018-09-18 11:37:17 -0300

    aula 123

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,7 +20,7 @@ export const loginUser = ({ email, password }) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => loginUserSuccess(dispatch, user))
       .catch(() => {
-        firebase.auth().createUserWithEmailAndPAssword(email, password)
+        firebase.auth().createUserWithEmailAndPassword(email, password)
           .then(user => loginUserSuccess(dispatch, user))
           .catch(() => loginUserFail(dispatch));
       }); //*1
@@ -28,11 +28,13 @@ export const loginUser = ({ email, password }) => {
 };
 
 const loginUserFail = (dispatch) => {
-  dispatch({ type: LOGIN_USER_FAIL });
+  dispatch({
+    type: LOGIN_USER_FAIL
+  });
 };
 
 const loginUserSuccess = (dispatch, user) => {
-    dispatch({ 
+    dispatch({
       type: LOGIN_USER_SUCCESS,
       payload: user
     });
